9424fcb1d515bf4c2ae24008be31d55762784684
dsomasundar2 <dsomasundar2@CSCINDAI408872.asiapac.globalcsc.net>
2017-03-10 14:47:38 +0530

    First Complete Commit

---
+++ src/main/java/com/git/session/GitDemoSessionApplication.java
@@ -0,0 +1,12 @@
+package com.git.session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoSessionApplication.class, args);
+	}
+}
---
+++ src/test/java/com/git/session/GitDemoSessionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.git.session;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitDemoSessionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
