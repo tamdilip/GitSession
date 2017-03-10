0e609d92b2fd4f335ca84438063a45a6d3561f68
bnarayanan20 <bnarayanan20@CSCINDAE712617.asiapac.globalcsc.net>
2017-03-10 16:53:28 +0530

    Eclipse change

--- src/main/java/com/git/session/GitDemoSessionApplication.java
+++ src/main/java/com/git/session/GitDemoSessionApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitDemoSessionApplication {
 //Hello
+	//Stash 
 	public static void main(String[] args) {
 		SpringApplication.run(GitDemoSessionApplication.class, args);
 	}
