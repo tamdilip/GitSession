299a66edc003e26ff65d5ddb82841691723fd02f
bnarayanan20 <bnarayanan20@CSCINDAE712617.asiapac.globalcsc.net>
2017-03-10 17:00:51 +0530

    Merged conflicts

--- src/main/java/com/git/session/GitDemoSessionApplication.java
+++ src/main/java/com/git/session/GitDemoSessionApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitDemoSessionApplication {
 //Hello
 	//Stash 
+	//Change from github
 	public static void main(String[] args) {
 		SpringApplication.run(GitDemoSessionApplication.class, args);
 	}
