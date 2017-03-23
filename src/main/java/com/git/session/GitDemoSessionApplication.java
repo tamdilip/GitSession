ae144c4290dd86f7b70443b5ddee5775b7ebbbb1
dsomasundar2 <dsomasundar2@CSCINDAI408872.asiapac.globalcsc.net>
2017-03-23 12:00:46 +0530

    Stash

--- src/main/java/com/git/session/GitDemoSessionApplication.java
+++ src/main/java/com/git/session/GitDemoSessionApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitDemoSessionApplication {
 //Fixed
+//ToStash
 	public static void main(String[] args) {
 		SpringApplication.run(GitDemoSessionApplication.class, args);
 		System.out.println("Test12 Added");
