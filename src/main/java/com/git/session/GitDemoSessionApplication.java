26b234f52b78e5c555acccf24a2f026e32178cff
tamdilip <tamdilip@gmail.com>
2017-03-23 11:22:44 +0530

    Update GitDemoSessionApplication.java

--- src/main/java/com/git/session/GitDemoSessionApplication.java
+++ src/main/java/com/git/session/GitDemoSessionApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitDemoSessionApplication {
-//Hello
-//TestF1
+
 	public static void main(String[] args) {
 		SpringApplication.run(GitDemoSessionApplication.class, args);
 		System.out.println("Test1 Added");
