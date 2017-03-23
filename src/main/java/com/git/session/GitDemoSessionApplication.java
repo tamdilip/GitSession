71bd3907975f723a0f915038730fa2591eac1352
dsomasundar2 <dsomasundar2@CSCINDAI408872.asiapac.globalcsc.net>
2017-03-23 10:49:12 +0530

    Test

--- src/main/java/com/git/session/GitDemoSessionApplication.java
+++ src/main/java/com/git/session/GitDemoSessionApplication.java
@@ -5,12 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitDemoSessionApplication {
-<<<<<<< HEAD
 //Hello
-=======
 //TestF1
->>>>>>> refs/remotes/origin/Feature-1
-//Fixed
+
 	public static void main(String[] args) {
 		SpringApplication.run(GitDemoSessionApplication.class, args);
 	}
