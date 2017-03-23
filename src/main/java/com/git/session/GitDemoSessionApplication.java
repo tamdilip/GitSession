e4df4ca7cee02b46024c8a20484042883c20150d
tamdilip <tamdilip@gmail.com>
2017-03-23 10:53:25 +0530

    Update GitDemoSessionApplication.java

--- src/main/java/com/git/session/GitDemoSessionApplication.java
+++ src/main/java/com/git/session/GitDemoSessionApplication.java
@@ -9,5 +9,6 @@ public class GitDemoSessionApplication {
 //Fixed
 	public static void main(String[] args) {
 		SpringApplication.run(GitDemoSessionApplication.class, args);
+		System.out.println("Test Added");
 	}
 }
